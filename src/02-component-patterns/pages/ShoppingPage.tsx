5dd1c1cc541500186d354dec149634082ed05277
eduviana <eduardoviana83@gmail.com>
2023-01-26 16:46:26 -0300

    pre npm listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -5,7 +5,7 @@ import {
   ProductTitle,
 } from "../components/index";
 import { products } from "../data/products";
-import "../styles/custom-styles.css";
+
 
 const product = products[0];
 
@@ -18,7 +18,6 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 6,
           maxCount: 10,
@@ -26,16 +25,9 @@ export const ShoppingPage = () => {
       >
         {({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
           <>
-            <ProductImage className="custom-image" />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {
-              (!isMaxCountReached && <button onClick={() => increaseBy(+2)}>+2</button> )
-            }
-            
-            <span>{count} - {maxCount}</span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
